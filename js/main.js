bb87c28348ab13cdcf17300d92a505abbbf243fc
viksridhar <vik.sridhar@gmail.com>
2019-02-18 23:04:46 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -70,7 +70,7 @@ return {
 				username = session.username;
 				credentials = session;
 				sessionid = session.sessionId;
-				api.getCurrentCredentials(function (currentCredentials) {console.log(currentCredentials)});
+				api.getCurrentCredentials(function (currentCredentials) {server = currentCredentials});
 
 				api.call("Get", {
 					"typeName" : "User",
